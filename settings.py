3fc1eb4e67b5c28995fe32e7711212d8049d57b1
jaysterlozano <44782539+jaysterlozano@users.noreply.github.com>
2018-12-12 22:29:20 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,46 @@
+TITLE = "Astro"
+# screen dims
+WIDTH = 480
+HEIGHT = 600
+# frames per second
+FPS = 60
+# colors
+WHITE = (0, 153, 0)
+BLACK = (0,0,0)
+REDDISH = (255,255,153)
+SKY_BLUE = (143, 185, 252)
+FONT_NAME = 'comic sans'
+SPRITESHEET = "spritesheet_jumper.png"
+SPRITESHEET2 = "spritesheet.png"
+# data files
+HS_FILE = "highscore.txt"
+# player settings
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
+# game settings
+BOOST_POWER = 60
+POW_SPAWN_PCT = 10
+MOB_FREQ = 600
+# layers - uses numerical value in layered sprites
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
+MOB_LAYER = 2
+CLOUD_LAYER = 0
+
+# platform settings
+''' old platforms from drawing rectangles'''
+'''
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
+'''
+PLATFORM_LIST = [(0, HEIGHT - 40),
+                 (65, HEIGHT - 300),
+                 (20, HEIGHT - 350),
+                 (200, HEIGHT - 150),
+                 (200, HEIGHT - 450)]
